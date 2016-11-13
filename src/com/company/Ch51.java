d934d39c01580731efc30f31a21bb5bd0a56b4d3
Valter Melon <kevooi.of.estland@gmail.com>
2016-11-13 03:43:08 +0200

    Initial commit

---
+++ src/com/company/Ch51.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Ch51 {
+
+    public static void main(String[] args) {
+        int x = 4;
+        if (x > 0 || x < 10) {
+            System.out.println("positive single digit number.");
+        }
+    }
+}
